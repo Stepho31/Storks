8f4c5f49fbcf82f336c765d6c7dbbc9c672de126
Stephan Dowless <slcd09@yahoo.com>
2023-08-08 14:19:49 -0400

    Initial Commit

---
+++ Tinder/TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApp.swift
+//  Tinder
+//
+//  Created by Stephan Dowless on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

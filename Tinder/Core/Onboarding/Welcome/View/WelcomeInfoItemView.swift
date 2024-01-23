8caf4e828496516f9ae1f205b89d6cb07c3f5537
Stephan Dowless <slcd09@yahoo.com>
2024-01-23 12:40:29 -0500

    Onboarding folder

---
+++ Tinder/Core/Onboarding/Welcome/View/WelcomeInfoItemView.swift
@@ -0,0 +1,34 @@
+//
+//  WelcomeInfoItemView.swift
+//  Tinder
+//
+//  Created by Stephan Dowless on 1/23/24.
+//
+
+import SwiftUI
+
+struct WelcomeInfoItemView: View {
+    let title: String
+    let subtitle: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(title)
+                .fontWeight(.semibold)
+            
+            Text(subtitle)
+                .foregroundStyle(.gray)
+                
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .font(.subheadline)
+    }
+}
+
+#Preview {
+    Group {
+        WelcomeInfoItemView(title: "Be yourself.", subtitle: "Make sure your photos, age, and bio are true to who you are.")
+        WelcomeInfoItemView(title: "Be proactive.", subtitle: "Always report bad behavior.")
+    }
+
+}

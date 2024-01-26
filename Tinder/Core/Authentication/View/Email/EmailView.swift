3f6921deaa5caf704eec4f0033a10b83d6632134
Brandon Dowless <dowlessbrandon@gmail.com>
2024-01-26 11:36:32 -0500

    Created email view

---
+++ Tinder/Core/Authentication/View/Email/EmailView.swift
@@ -0,0 +1,18 @@
+//
+//  EmailView.swift
+//  Tinder
+//
+//  Created by Brandon on 1/26/24.
+//
+
+import SwiftUI
+
+struct EmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmailView()
+}

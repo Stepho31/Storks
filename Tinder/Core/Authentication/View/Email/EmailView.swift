7c04a34554803ccb4f681f5d76fa6513b51cbeaa
Brandon Dowless <dowlessbrandon@gmail.com>
2024-01-26 15:35:02 -0500

    added logic to make sure the user can't continue unless requirements are met

--- Tinder/Core/Authentication/View/Email/EmailView.swift
+++ Tinder/Core/Authentication/View/Email/EmailView.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct EmailView: View {
+    @State var email = ""
+    
+     var userCanContinue: Bool {
+        if email.isValidEmail() {
+            return true
+        } else {
+            return false
+        }
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack {
+            VStack(spacing: 0) {
+                HStack {
+                    Button(action: {}, label: {
+                        Image(systemName: "chevron.left")
+                            .imageScale(.large)
+                            .fontWeight(.heavy)
+                            .foregroundStyle(.gray)
+                            .opacity(0.6)
+                    })
+                    Spacer()
+                }
+                
+                VStack(alignment: .leading, spacing: 16) {
+                    Text("Your email?")
+                        .bold()
+                        .font(.title)
+                    
+                    Text("Don't lose access to your account, add your email.")
+                        .font(.subheadline)
+                        .foregroundStyle(.gray)
+                    
+                    VStack(spacing: 10) {
+                        TextField("Enter email", text: $email)
+                        
+                        
+                        Divider()
+                            .frame(height: 1)
+                            .opacity(0.5)
+                    }
+                }
+                .padding(.horizontal, 28)
+                
+                Spacer()
+                
+                NavigationLink {
+                    Text("Password View")
+                } label: {
+                    Text("Next")
+                        .foregroundStyle(userCanContinue ?  Color(.white) : Color(.black).opacity(0.5))
+                        .bold()
+                        .font(.title3)
+                        .frame(width: 320, height: 50)
+                        .background(userCanContinue ?  Color(.primaryPink) : Color(.systemGray5))
+                        .clipShape(Capsule())
+                }
+                .disabled(!userCanContinue)
+            }
+            .navigationBarBackButtonHidden()
+            .padding(.horizontal, 20)
+            .padding(.vertical, 20)
+        }
     }
 }
 

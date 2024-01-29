1f16528ceee34f3e010285a903c8cd409313e6d0
Brandon Dowless <dowlessbrandon@gmail.com>
2024-01-29 14:09:37 -0500

    made requested changes

---
+++ Tinder/Core/Onboarding/Views/FullName/FullNameView.swift
@@ -0,0 +1,54 @@
+//
+//  Name.swift
+//  Tinder
+//
+//  Created by Brandon on 1/29/24.
+//
+
+import SwiftUI
+
+struct FullNameView: View {
+    @State var fullName: String = ""
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 20) {
+                
+                Text("What's your full name?")
+                    .font(.title)
+                    .bold()
+                
+                VStack(alignment: .leading) {
+                    TextField("Enter Full Name", text: $fullName)
+                    
+                    Divider()
+                    
+                    VStack(alignment: .leading) {
+                        Text("This is how it'll appear in your profile.")
+                            .font(.footnote)
+                            .opacity(0.6)
+                        
+                        Text("Can't change it later.")
+                            .font(.footnote)
+                            .bold()
+                    }
+                    .padding(.vertical, 6)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    BackButton()
+                }
+            }
+            .foregroundStyle(.white)
+            .background(.black)
+        }
+    }
+}
+
+#Preview {
+    FullNameView()
+}

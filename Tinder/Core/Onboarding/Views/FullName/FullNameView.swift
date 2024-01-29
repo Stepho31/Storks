9b5ad739459bb3ffc224279af9e74b907c9d1758
Brandon Dowless <dowlessbrandon@gmail.com>
2024-01-29 14:41:37 -0500

    made changes

--- Tinder/Core/Onboarding/Views/FullName/FullNameView.swift
+++ Tinder/Core/Onboarding/Views/FullName/FullNameView.swift
@@ -10,45 +10,43 @@ import SwiftUI
 struct FullNameView: View {
     @State var fullName: String = ""
     var body: some View {
-        NavigationStack {
-            VStack(alignment: .leading, spacing: 20) {
+        VStack(alignment: .leading, spacing: 20) {
+            
+            Text("What's your full name?")
+                .font(.title)
+                .bold()
+            
+            VStack(alignment: .leading) {
+                TextField("Enter Full Name", text: $fullName)
                 
-                Text("What's your full name?")
-                    .font(.title)
-                    .bold()
+                Divider()
                 
                 VStack(alignment: .leading) {
-                    TextField("Enter Full Name", text: $fullName)
+                    Text("This is how it'll appear in your profile.")
+                        .font(.footnote)
+                        .opacity(0.6)
                     
-                    Divider()
-                    
-                    VStack(alignment: .leading) {
-                        Text("This is how it'll appear in your profile.")
-                            .font(.footnote)
-                            .opacity(0.6)
-                        
-                        Text("Can't change it later.")
-                            .font(.footnote)
-                            .bold()
-                    }
-                    .padding(.vertical, 6)
+                    Text("Can't change it later.")
+                        .font(.footnote)
+                        .bold()
                 }
-                
-                Spacer()
+                .padding(.vertical, 6)
             }
-            .padding()
             
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    BackButton()
-                }
-            }
-            .foregroundStyle(.white)
-            .background(.black)
+            Spacer()
+        }
+        .padding()
+        .foregroundStyle(.white)
+        .background(.black)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) 
+            { BackButton() }
         }
     }
 }
 
 #Preview {
-    FullNameView()
+    NavigationStack {
+        FullNameView()
+    }
 }

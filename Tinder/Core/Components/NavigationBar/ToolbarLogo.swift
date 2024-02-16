4b08aff9a6a9b0911097f5605f9561cd39d24d7b
Stephan Dowless <slcd09@yahoo.com>
2024-02-16 09:40:01 -0500

    Logo update

--- Tinder/Core/Components/NavigationBar/ToolbarLogo.swift
+++ Tinder/Core/Components/NavigationBar/ToolbarLogo.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct ToolbarLogo: View {
     var body: some View {
-        Image(.tinderLogo)
-            .resizable()
-            .scaledToFill()
-            .frame(width: 88)
+//        Image(.tinderLogo)
+//            .resizable()
+//            .scaledToFill()
+//            .frame(width: 88)
+        
+        Text("AURORA")
+            .font(.title2)
+            .fontWeight(.semibold)
+            .foregroundStyle(.white)
     }
 }
 
